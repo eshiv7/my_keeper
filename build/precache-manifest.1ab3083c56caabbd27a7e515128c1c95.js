358f4bfff3703e2efa3959948b5c9faea1fc9193
shivraj <shivraj.rajput.777@gmail.com>
2022-10-28 18:49:55 +0530

    deployed

---
+++ build/precache-manifest.1ab3083c56caabbd27a7e515128c1c95.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d49ba8ca62a803f75d48aeb96ca2a4b",
+    "url": "/my_keeper/index.html"
+  },
+  {
+    "revision": "36df98cfedea4f0f47c6",
+    "url": "/my_keeper/static/js/2.240d069a.chunk.js"
+  },
+  {
+    "revision": "4a44db53ea0760b1d22d",
+    "url": "/my_keeper/static/js/main.6034d711.chunk.js"
+  },
+  {
+    "revision": "8af1a972cff3bae8cd78",
+    "url": "/my_keeper/static/js/runtime-main.0466d995.js"
+  }
+]);
